332e2aa705342c240f51c9c1a976c0a2cc353a38
amirkhan3636 <55018489+amirkhan3636@users.noreply.github.com>
2022-08-21 13:33:51 +0430

    Update inf.lua

--- inf.lua
+++ inf.lua
@@ -1,5 +1,1 @@
-game:GetService("UserInputService").JumpRequest:connect(function()
-    if InfiniteJumpEnabled then
-	game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass'Humanoid':ChangeState("Jumping")
-    end
-end)
+print('GasMask On TOP')
